12149e041c6732fd562374a2f6e4ae5ff6d4d23a
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2025-02-19 21:39:40 +0900

    Add new API models and update OpenAPI version to 3.3.29

---
+++ src/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './PublicAuthControllerMe200Response';
+export * from './PublicAuthControllerMe200ResponseMetadata';
+export * from './PublicAuthControllerMe200ResponseProviderDataInner';
+export * from './PublicAuthControllerSignin200Response';
+export * from './PublicAuthControllerSigninRequest';
+export * from './PublicAuthControllerSignupRequest';
